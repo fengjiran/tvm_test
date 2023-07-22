8d71c236db4206e2d43057ff1fd8f21d756ef2a6
richard <fengjiran@foxmail.com>
2023-07-22 21:15:51 +0800

    test tvm PackedFunc

---
+++ test_packedfunc.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 赵丹 on 2023/7/22.
+//
+
+#include "test_packedfunc.h"
---
+++ test_packedfunc.h
@@ -0,0 +1,8 @@
+//
+// Created by 赵丹 on 2023/7/22.
+//
+
+#ifndef TVM_TEST_TEST_PACKEDFUNC_H
+#define TVM_TEST_TEST_PACKEDFUNC_H
+
+#endif//TVM_TEST_TEST_PACKEDFUNC_H
