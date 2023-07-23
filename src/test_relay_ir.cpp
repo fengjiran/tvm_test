7dae544fd2ad02356db515bdd59ee13f97456009
richard <fengjiran@foxmail.com>
2023-07-24 00:34:21 +0800

    alloc NDArray

--- src/test_relay_ir.cpp
+++ src/test_relay_ir.cpp
@@ -3,17 +3,20 @@
 //
 
 #include "test_relay_ir.h"
-#include "tvm/runtime/registry.h"
-#include "tvm/relay/expr.h"
-#include <vector>
+
 #include <random>
+#include <vector>
+
+#include "tvm/relay/expr.h"
+#include "tvm/runtime/registry.h"
 
 using namespace tvm::runtime;
+using namespace tvm::relay;
 
-void random_matrix(int32_t *matrix, int rows, int cols) {
+void random_matrix(int64_t *matrix, int rows, int cols) {
     std::random_device rd;
     std::mt19937 gen(rd());
-    std::uniform_int_distribution<int32_t> dist(0, 20);
+    std::uniform_int_distribution<int64_t> dist(0, 20);
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
             matrix[i * cols + j] = dist(gen);
@@ -22,27 +25,40 @@ void random_matrix(int32_t *matrix, int rows, int cols) {
 }
 
 void test_constant() {
-    const char *name = "relay.ir.Constant";
-    const PackedFunc *fp = Registry::Get(name);
-    NDArray x = NDArray::Empty(
-            {4, 3},
-            DLDataType{kDLFloat, 32, 1},
-            DLDevice{kDLCPU, 0}
-    );
+    //    const char *name = "relay.ir.Constant";
+    //    const PackedFunc *fp = Registry::Get(name);
+    //    NDArray xx = NDArray::Empty(
+    //            {4, 3},
+    //            DLDataType{kDLFloat, 32, 1},
+    //            DLDevice{kDLCPU, 0}
+    //    );
 
     int rows = 4;
     int cols = 3;
-    auto *matrix = new int32_t[rows * cols];
-    random_matrix(matrix, rows, cols);
+    auto *data = new int64_t[rows * cols];
+    random_matrix(data, rows, cols);
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
-            std::cout << matrix[i * cols + j] << " ";
+            std::cout << data[i * cols + j] << " ";
         }
         std::cout << std::endl;
     }
 
-    DLTensor tet {matrix};
+    DLTensor tensor;
+    ShapeTuple shape({rows, cols});
+    tensor.data = data;
+    tensor.ndim = static_cast<int>(shape.size());
+    tensor.shape = const_cast<ShapeTuple::index_type *>(shape.data());
+    tensor.dtype = DLDataType{kDLInt, 64, 1};
+    tensor.strides = nullptr;
+    tensor.byte_offset = 0;
+    tensor.device = DLDevice{kDLCPU, 0};
+    NDArray x = NDArray::FromExternalDLTensor(tensor);
 
-    delete[] matrix;
+    const char *name = "relay.ir.Constant";
+    const PackedFunc *fp = Registry::Get(name);
 
+    Constant constant = (*fp)(x, Span());
+
+    delete[] data;
 }
