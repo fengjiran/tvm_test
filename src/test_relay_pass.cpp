12b22465344b292362476aabc940d110d770233a
fengjiran <fengjiran@foxmail.com>
2023-08-17 12:48:43 +0800

    add op strategy test file

--- src/test_relay_pass.cpp
+++ src/test_relay_pass.cpp
@@ -5,10 +5,7 @@
 #include "tvm/relay/expr.h"
 #include "tvm/relay/op.h"
 #include "tvm/relay/attrs/on_device.h"
-#include "tvm/relay/op_attr_types.h"
-#include "tvm/relay/op_strategy.h"
 #include "tvm/target/virtual_device.h"
-#include "tvm/topi/broadcast.h"
 #include "tvm/topi/generic/injective.h"
 #include "build_relay_model.h"
 
@@ -99,41 +96,6 @@ bool IsComplexConstant(const Expr &expr) {
     }
 }
 
-TVM_REGISTER_GLOBAL("test.add_strategy")
-.set_body_typed([](const Attrs& attrs, const Array<te::Tensor>& inputs, const Type& out_type,
-                   const Target& target) {
-    FTVMCompute fcompute = [](const Attrs& attrs, const Array<te::Tensor>& inputs,
-                              const Type& out_type) -> Array<te::Tensor> {
-        ICHECK_EQ(inputs.size(), 2U);
-        return {topi::add(inputs[0], inputs[1])};
-    };
-    FTVMSchedule fschedule = [](const Attrs& attrs, const Array<te::Tensor>& outs,
-                                const Target& target) {
-        With<Target> target_scope(target);
-        return topi::generic::schedule_injective(target, outs);
-    };
-
-    auto n = make_object<OpStrategyNode>();
-    auto strategy = relay::OpStrategy(std::move(n));
-    strategy.AddImplementation(fcompute, fschedule, "test.add_strategy", 10);
-    return strategy;
-});
-
-TVM_REGISTER_GLOBAL("relay.backend.lower_call")
-.set_body_typed([](const relay::Call& call, const Array<te::Tensor>& inputs,
-                   const Target& target) {
-    static auto fstrategy = Op::GetAttrMap<relay::FTVMStrategy>("FTVMStrategy");
-    Op op = Downcast<Op>(call->op);
-    auto out_type = call->checked_type();
-    OpStrategy strategy = fstrategy[op](call->attrs, inputs, out_type, target);
-    auto impl = strategy->specializations[0]->implementations[0];
-    auto outs = impl.Compute(call->attrs, inputs, out_type);
-    auto f = runtime::Registry::Get("relay.backend._make_LoweredOutput");
-    if (!f) {
-        LOG(FATAL) << "relay.backend._make_LoweredOutput is not registered";
-    }
-    return (*f)(outs, impl);
-});
 
 TEST(RelayPass, ConstantCheck) {
     relay::Constant c1 = relay::Constant(runtime::NDArray::Empty({1, 16, 64, 64},
