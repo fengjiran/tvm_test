cfc393ddc628cdf3b5f39e021569be7ce6719319
fengjiran <fengjiran@foxmail.com>
2023-08-24 17:11:39 +0800

    test sort func

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,6 @@
 #include "gtest/gtest.h"
 
-
 int main() {
-    std::vector<std::vector<int>> edges{{0, 1, 2},
-                                        {2, 3, 8},
-                                        {3, 4, 1}};
-    std::sort(edges.begin(), edges.end(),
-              [](std::vector<int> a, std::vector<int> b) { return a[2] < b[2]; });
-    for (auto & edge : edges){
-        std::cout<< edge[0] << ' ' << edge[1] << ' ' << edge[2] << std::endl;
-    }
-
     testing::InitGoogleTest();
     return RUN_ALL_TESTS();
 }
--- src/test_leetcode.cpp
+++ src/test_leetcode.cpp
@@ -1,3 +1,15 @@
 //
 // Created by 赵丹 on 2023/8/24.
 //
+#include "gtest/gtest.h"
+
+TEST(Sort, SortCmpFunc) {
+    std::vector<std::vector<int>> edges{{0, 1, 2},
+                                        {2, 3, 8},
+                                        {3, 4, 1}};
+    std::sort(edges.begin(), edges.end(),
+              [](std::vector<int>& a, std::vector<int>& b) { return a[2] < b[2]; });
+    for (auto & edge : edges){
+        std::cout<< edge[0] << ' ' << edge[1] << ' ' << edge[2] << std::endl;
+    }
+}
