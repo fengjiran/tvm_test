83973a1d2a86854f120f2c7eb082f714296aa60c
fengjiran <fengjiran@foxmail.com>
2023-08-31 01:26:31 +0800

    test topi op

---
+++ include/test_topi_op.h
@@ -0,0 +1,8 @@
+//
+// Created by 赵丹 on 2023/8/31.
+//
+
+#ifndef TVM_TEST_TEST_TOPI_OP_H
+#define TVM_TEST_TEST_TOPI_OP_H
+
+#endif //TVM_TEST_TEST_TOPI_OP_H
---
+++ src/test_topi.cpp
@@ -0,0 +1,4 @@
+//
+// Created by 赵丹 on 2023/8/31.
+//
+#include "test_topi_op.h"
