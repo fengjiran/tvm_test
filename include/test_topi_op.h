c3dc5121306ac50b16aeb8a5f1e4b7eac8950970
fengjiran <fengjiran@foxmail.com>
2023-08-31 01:40:22 +0800

    test topi relu op

--- include/test_topi_op.h
+++ include/test_topi_op.h
@@ -5,4 +5,16 @@
 #ifndef TVM_TEST_TEST_TOPI_OP_H
 #define TVM_TEST_TEST_TOPI_OP_H
 
+#include "tvm/te/operation.h"
+#include "tvm/tir/expr.h"
+#include "tvm/tir/op.h"
+#include "tvm/topi/tags.h"
+
+template<typename T>
+inline tvm::te::Tensor relu(const tvm::te::Tensor &data, T threshold = static_cast<T>(0),
+                            const std::string &name = "T_relu",
+                            const std::string &tag = tvm::topi::kElementWise) {
+    //
+}
+
 #endif //TVM_TEST_TEST_TOPI_OP_H
