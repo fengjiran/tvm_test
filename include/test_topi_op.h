41d6cd9894944574e17e83e6decd0fa884c7aeda
fengjiran <fengjiran@foxmail.com>
2023-08-31 12:47:25 +0800

    test topi relu

--- include/test_topi_op.h
+++ include/test_topi_op.h
@@ -14,7 +14,11 @@ template<typename T>
 inline tvm::te::Tensor relu(const tvm::te::Tensor &data, T threshold = static_cast<T>(0),
                             const std::string &name = "T_relu",
                             const std::string &tag = tvm::topi::kElementWise) {
-    //
+    auto fcompute = [&](const tvm::Array<tvm::tir::Var> &indices) {
+        auto threshold_const = tvm::tir::make_const(data->dtype, threshold);
+        return tvm::max(data(indices), threshold_const);
+    };
+    return tvm::te::compute(data->shape, fcompute, name, tag);
 }
 
 #endif //TVM_TEST_TEST_TOPI_OP_H
--- src/test_topi.cpp
+++ src/test_topi.cpp
@@ -1,4 +1,9 @@
 //
 // Created by 赵丹 on 2023/8/31.
 //
+#include "gtest/gtest.h"
 #include "test_topi_op.h"
+
+TEST(TOPI, Relu) {
+    //
+}
