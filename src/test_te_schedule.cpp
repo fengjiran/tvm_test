970a6cefb5c636c133c62f43cccffc4f554cb2b5
fengjiran <fengjiran@foxmail.com>
2023-09-04 21:18:23 +0800

    test te schedule of split

--- src/test_te_schedule.cpp
+++ src/test_te_schedule.cpp
@@ -7,9 +7,14 @@
 #include "tvm/te/schedule.h"
 #include "tvm/tir/var.h"
 #include "tvm/tir/op.h"
+#include "tvm/topi/reduction.h"
 
 using namespace tvm;
 
 TEST(TESchedule, split) {
-    //
+    int n = 1024;
+    auto A = te::placeholder(Array<PrimExpr>{n}, DataType::Float(32), "A");
+    auto T = topi::sum(A, {0});
+    auto s = te::create_schedule(Array<te::Operation>{T->op});
+//    auto x = s[T].split()
 }
