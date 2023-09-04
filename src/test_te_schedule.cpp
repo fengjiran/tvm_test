e60b0a2c580de6bbb10634658228fde8fd875408
fengjiran <fengjiran@foxmail.com>
2023-09-04 08:45:17 +0800

    add te schedule test file

--- src/test_te_schedule.cpp
+++ src/test_te_schedule.cpp
@@ -1,3 +1,9 @@
 //
 // Created by richard on 9/3/23.
 //
+#include "gtest/gtest.h"
+#include "tvm/te/tensor.h"
+#include "tvm/te/operation.h"
+#include "tvm/te/schedule.h"
+#include "tvm/tir/var.h"
+#include "tvm/tir/op.h"
