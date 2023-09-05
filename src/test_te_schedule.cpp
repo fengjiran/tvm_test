1ac61618090129c32513f699096c87d9e60323a3
fengjiran <fengjiran@foxmail.com>
2023-09-05 17:35:08 +0800

    test two axes split

--- src/test_te_schedule.cpp
+++ src/test_te_schedule.cpp
@@ -13,15 +13,22 @@
 using namespace tvm;
 
 TEST(TESchedule, split) {
+    int m = 1024;
     int n = 1024;
-    auto A = te::placeholder(Array<PrimExpr>{n}, DataType::Float(32), "A");
-    auto T = topi::sum(A, {0});
+    int c = 10;
+    auto A = te::placeholder(Array<PrimExpr>{m, n, c}, DataType::Float(32), "A");
+    auto T = topi::sum(A, {0, 1});
     auto s = te::create_schedule(Array<te::Operation>{T->op});
     std::cout << "stage num: " << s->stages.size() << std::endl;
-    tir::IterVar outer;
-    tir::IterVar inner;
-    auto reduce_axis = Downcast<te::ComputeOp>(T->op)->reduce_axis[0];
-    auto stage = s[T].split(reduce_axis, 16, &outer, &inner);
+    auto reduce_axes = Downcast<te::ComputeOp>(T->op)->reduce_axis;
+    ASSERT_EQ(reduce_axes.size(), 2);
+
+    tir::IterVar outer0;
+    tir::IterVar inner0;
+    tir::IterVar outer1;
+    tir::IterVar inner1;
+    auto stage = s[T].split(reduce_axes[0], 16, &outer0, &inner0)
+            .split(reduce_axes[1], 16, &outer1, &inner1);
     auto mod = LowerSchedule(s, Array<te::Tensor>{A, T}, "main", {}, GlobalVarSupply(NameSupply("")), true);
     std::cout << mod << std::endl;
 }
