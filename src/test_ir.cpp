b3477f6122232688c4e76b634c35bae21f315582
fengjiran <fengjiran@foxmail.com>
2023-09-25 00:24:27 +0800

    test name supply

--- src/test_ir.cpp
+++ src/test_ir.cpp
@@ -5,3 +5,10 @@
 #include "tvm/ir/name_supply.h"
 
 using namespace tvm;
+
+TEST(IR, NameSupply) {
+    std::string prefix = "test";
+    std::unordered_map<std::string, int> name_map;
+    auto name_supply = NameSupply(prefix, name_map);
+
+}
