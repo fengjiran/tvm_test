87ee0fec2b08070ba708d7eaa83e0d90005b92bf
fengjiran <fengjiran@foxmail.com>
2023-09-25 09:28:10 +0800

    test insert

--- src/test_ir.cpp
+++ src/test_ir.cpp
@@ -6,11 +6,15 @@
 
 using namespace tvm;
 
-TEST(IR, NameSupply) {
+TEST(IR, MapInsertFunc) {
     std::string prefix = "test";
     std::unordered_map<std::string, int> name_map;
-    auto name_supply = NameSupply(prefix, name_map);
+//    auto name_supply = NameSupply(prefix, name_map);
     auto x = name_map.insert({prefix, 0});
     ASSERT_EQ(x.first->first, "test");
     ASSERT_EQ(x.second, true);
+
+    auto y = name_map.insert({prefix, 1});
+    ASSERT_EQ(y.first->second, 0);
+    ASSERT_EQ(y.second, false);
 }
