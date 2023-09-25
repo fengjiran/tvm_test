2aeb4708baeeb558c387ee7680680de5a35978c2
fengjiran <fengjiran@foxmail.com>
2023-09-25 09:22:30 +0800

    test insert

--- src/test_ir.cpp
+++ src/test_ir.cpp
@@ -10,5 +10,7 @@ TEST(IR, NameSupply) {
     std::string prefix = "test";
     std::unordered_map<std::string, int> name_map;
     auto name_supply = NameSupply(prefix, name_map);
-
+    auto x = name_map.insert({prefix, 0});
+    ASSERT_EQ(x.first->first, "test");
+    ASSERT_EQ(x.second, true);
 }
